271a060fbceba2b9f52ad615d5850c17cf5e69fc
benisai <59147467+benisai@users.noreply.github.com>
2022-10-25 13:10:49 -0700

    Update routersetup.sh

--- routersetup.sh
+++ routersetup.sh
@@ -14,8 +14,8 @@ HOMESERVER=0.0.0.0
 # === Copying nat_traffic.lua and app-statistics Files from GIT =============
  echo 'Copying nat_traffic.lua from /benisai/Openwrt-Monitoring/nat_traffic.lua'
  wget https://raw.githubusercontent.com/benisai/Openwrt-Monitoring/main/nat_traffic.lua -O /usr/lib/lua/prometheus-collectors/nat_traffic.lua
-# === echo 'Copying nat_traffic.lua from /benisai/Openwrt-Monitoring/nat_traffic.lua'
-# === wget https://raw.githubusercontent.com/benisai/Openwrt-Monitoring/main/nat_traffic.lua -O /etc/config/luci_statistics
+ echo 'Copying luci_statistics from /benisai/Openwrt-Monitoring/luci_statistics'
+ wget https://raw.githubusercontent.com/benisai/Openwrt-Monitoring/main/Router/luci_statistics -O /etc/config/luci_statistics
 # === Setting up app-statistics and prometheus configs =============
  echo 'updating prometheus config from loopback to lan'
  sed -i 's/loopback/lan/g'  /etc/config/prometheus-node-exporter-lua
