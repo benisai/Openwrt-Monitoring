3315e08c35b4097a60d253e6e1df0d9d186ace3f
benisai <59147467+benisai@users.noreply.github.com>
2022-11-15 19:48:48 -0800

    Update speedtest.lua

--- Router/speedtest.lua
+++ Router/speedtest.lua
@@ -1,9 +1,8 @@
 local function scrape()
   local speedt = space_split(get_contents("/tmp/speedtest.out"))
 
-  metric("node_load1", "gauge", nil, speedt[1])
-  metric("node_load5", "gauge", nil, speedt[2])
-  metric("node_load15", "gauge", nil, speedt[3])
+  metric("router_speedtest_download", "gauge", nil, speedt[1])
+  metric("router_speedtest_upload", "gauge", nil, speedt[2])
 end
 
 return { scrape = scrape }
