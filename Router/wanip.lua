f924e70378741863a61b8ce79a55af697bc0f354
benisai <59147467+benisai@users.noreply.github.com>
2022-11-20 17:44:58 -0800

    Update wanip.lua

--- Router/wanip.lua
+++ Router/wanip.lua
@@ -1,9 +1,8 @@
 local function scrape()
-  local speedt = space_split(get_contents("/tmp/wanip.out"))
-
-  metric("router_wan_ip", "gauge", nil, speedt[1])
-  metric("router_public_ip", "gauge", nil, speedt[2])
-  metric("router_internet_status", "gauge", nil, speedt[3])
+  local wantest = space_split(get_contents("/tmp/wanip.out"))
+  metric("router_wan_ip", "gauge", nil, wantest[1])
+  metric("router_public_ip", "gauge", nil, wantest[2])
+  metric("router_internet_status", "gauge", nil, wantest[3])
 end
 
 return { scrape = scrape }
