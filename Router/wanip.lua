a86aa1159408e6add3ddd576929025e075eea1da
benisai <59147467+benisai@users.noreply.github.com>
2022-11-22 13:27:33 -0800

    Update wanip.lua

--- Router/wanip.lua
+++ Router/wanip.lua
@@ -1,8 +1,23 @@
 local function scrape()
-  local wantest = space_split(get_contents("/tmp/wanip.out"))
-  metric("router_wan_ip", "gauge", nil, wantest[1])
-  metric("router_public_ip", "gauge", nil, wantest[2])
-  metric("router_internet_status", "gauge", nil, wantest[3])
+  wanip_metrics =  metric("wan_info", "gauge" )
+  for e in io.lines("/tmp/wanip.out") do
+    local fields = space_split(e)
+    local wanip, publicip, internet;
+    internet = 0;
+    for _, field in ipairs(fields) do
+      if wanip == nil and string.match(field, '^wanip') then
+        wanip = string.match(field,"wanip=([^ ]+)");
+      elseif publicip == nil and string.match(field, '^publicip') then
+        publicip = string.match(field,"publicip=([^ ]+)");
+      elseif string.match(field, '^internet') then
+        local b = string.match(field, "internet=([^ ]+)");
+        internet = internet + b;
+      end
+
+    end
+     local labels = { wanip = wanip, publicip = publicip }
+		    wanip_metrics(labels, internet )
+  end
 end
 
 return { scrape = scrape }
