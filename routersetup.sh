d29eeef26cc444c6d0416a58d1bed7a81a0c1ec0
benisai <59147467+benisai@users.noreply.github.com>
2022-11-26 22:02:49 -0800

    Update routersetup.sh

--- routersetup.sh
+++ routersetup.sh
@@ -28,28 +28,41 @@ HOMESERVER="10.0.5.5"
  opkg install /root/iptmon_0.1.6-1_all.ipk
  
  ipt=$(uci show dhcp.@dnsmasq[0].dhcpscript | grep "iptmon")
-if [[ -z "$ipt" ]]; then
+ if [[ -z "$ipt" ]]; then
   echo "Adding iptmon to DHCPScript option"
         uci set dhcp.@dnsmasq[0].dhcpscript=/usr/sbin/iptmon
         uci commit
         echo '/usr/sbin/iptmon init' >> /etc/firewall.user
         elif [[ -n "$ipt" ]]; then
   echo "IPTMon was found, no changes made to DHCP"
-fi
+ fi
+
  
  echo 'Copy shell scripts Speedtest.sh and wanip Script from Github/benisai/Openwrt-Monitoring/Router/'
  wget https://raw.githubusercontent.com/benisai/Openwrt-Monitoring/main/Router/speedtest.sh -O /usr/bin/speedtest.sh
- wget https://raw.githubusercontent.com/benisai/Openwrt-Monitoring/main/Router/1-min-check -O /usr/bin/1-min-check.sh
- 
+ wget https://raw.githubusercontent.com/benisai/Openwrt-Monitoring/main/Router/1-min-check.sh -O /usr/bin/1-min-check.sh
+ wget https://raw.githubusercontent.com/benisai/Openwrt-Monitoring/main/Router/new_device.sh -O /usr/bin/new_device.sh
+
+ #Adding new_device.sh script to dhcp dnsmasq
+ echo 'Adding new_device.sh script to dhcp dnsmasq.conf'
+ if [[ -z "$new_device.sh" ]]; then
+  echo "Adding new_device.sh to DHCPScript option"
+        uci set dhcp.@dnsmasq[0].dhcpscript=/usr/sbin/new_device.sh
+        uci commit
+        elif [[ -n "$new_device.sh" ]]; then
+  echo "Script New_Device.sh was found, no changes made to DHCP"
+ fi
+
+
  echo 'Add speedtest.sh to crontab'
  C=$(crontab -l | grep "speedtest.sh")
-if [[ -z "$C" ]]; then
+ if [[ -z "$C" ]]; then
    echo "Adding Speedtest.sh to crontab"
    crontab -l | { cat; echo "0 0 * * * /usr/bin/speedtest.sh > /tmp/speedtest.out"; } | crontab -
    crontab -l | { cat; echo "10 0 * * * rm -rf /tmp/speedtest.out"; } | crontab -
    elif [[ -n "$C" ]]; then
    echo "speedtest.sh was found in crontab"
-fi
+ fi
 
  
 # === Copying nat_traffic.lua and app-statistics Files from GIT =============
@@ -74,13 +87,13 @@ fi
 
 # === Setting up DNS ===========
 L=$(uci show dhcp.lan.dhcp_option | grep "$HOMESERVER")
-if [[ -z "$L" ]]; then
+ if [[ -z "$L" ]]; then
   echo "Adding $HOMESERVER DNS entry to LAN Interface"
   uci add_list dhcp.lan.dhcp_option="6,${HOMESERVER}"
   uci commit dhcp
-elif [[ -n "$L" ]]; then
+ elif [[ -n "$L" ]]; then
   echo "${HOMESERVER} DNS was found, no changes to DNS"
-fi
+ fi
 
 # === Setting Services to enable and restarting Services =============
  echo 'restarting services'
