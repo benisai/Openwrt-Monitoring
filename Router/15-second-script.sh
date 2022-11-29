e0195767e604a8e1aedbd283a189c3c243f2c3fd
benisai <59147467+benisai@users.noreply.github.com>
2022-11-28 17:36:04 -0800

    Update 15-second-script.sh

--- Router/15-second-script.sh
+++ Router/15-second-script.sh
@@ -3,4 +3,4 @@
 #wrtbwmon script will run every
 wrtbwmon publish /tmp/usage.db /tmp/usage.htm
 sleep 1
-cat /tmp/usage.htm | grep "2022" | sed 's/,/  /g; s/"//g; s/new Array//g' | tr -d '()' | sed '$d' > usage.out
+cat /tmp/usage.htm | grep "2022" | sed 's/,/  /g; s/"//g; s/new Array//g' | tr -d '()' | sed '$d' > /tmp/bwmon_usage.out
