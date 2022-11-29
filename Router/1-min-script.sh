95569829229ef3a3986c33748f84e0d8bd68a32d
benisai <59147467+benisai@users.noreply.github.com>
2022-11-28 17:38:27 -0800

    Update 1-min-script.sh

--- Router/1-min-script.sh
+++ Router/1-min-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ####PacketLoss
-packet=$(ping -c 45 8.8.8.8 | grep "packet loss" | awk -F ',' '{print $3}' | awk '{print $1}' | sed 's/%//g')
+packet=$(ping -c 20 8.8.8.8 | grep "packet loss" | awk -F ',' '{print $3}' | awk '{print $1}' | sed 's/%//g')
 echo "$packet" > /tmp/packetloss.out
 
 sleep 1
