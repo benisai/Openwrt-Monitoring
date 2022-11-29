6396707ebd503db05ef27674cb4bdb22e1b58438
benisai <59147467+benisai@users.noreply.github.com>
2022-11-28 17:13:32 -0800

    Update 1-min-script.sh

--- Router/1-min-script.sh
+++ Router/1-min-script.sh
@@ -15,7 +15,7 @@ echo "wanip=${WAN_ADDR}" "publicip=${cip}" >/tmp/wanip.out
 
 sleep 1
 #####Run vnstat and parse output
-vnstat --xml |grep -hnr "month id" | sed 's/<[^>]*>/ /g; s/2022//g; s/        //g' | cut -d " " -f2- > monthoutput.xml#######$4433
-vnstat --xml |grep -hnr "day id" | sed 's/<[^>]*>/ /g; s/2022//g; s/        //g' | cut -d " " -f2- > dayoutput.xml
-vnstat --xml |grep -hnr "hour id" | sed 's/<[^>]*>/ /g; s/2022//g; s/        //g; s/  00/:00/g; s/  /-/g' | cut -d " " -f2-  > houroutput.xml
-vnstat --xml |grep -hnr "fiveminute id" | sed 's/<[^>]*>/ /g; s/2022//g; s/        //g' | cut -d " " -f2-   > fiveoutput.xml
+vnstat --xml |grep -hnr "month id" | sed 's/<[^>]*>/ /g; s/2022//g; s/        //g' | cut -d " " -f2- > /tmp/monthoutput.out
+vnstat --xml |grep -hnr "day id" | sed 's/<[^>]*>/ /g; s/2022//g; s/        //g' | cut -d " " -f2- > /tmp/dayoutput.out
+vnstat --xml |grep -hnr "hour id" | sed 's/<[^>]*>/ /g; s/2022//g; s/        //g; s/  00/:00/g' | cut -d " " -f2-  > /tmp/houroutput.out
+vnstat --xml |grep -hnr "fiveminute id" | sed 's/<[^>]*>/ /g; s/2022//g; s/        //g' | cut -d " " -f2-   > /tmp/fiveoutput.out
