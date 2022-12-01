08c048a9b3edefdf123428d2061a1ff0497b59a6
benisai <59147467+benisai@users.noreply.github.com>
2022-11-30 17:12:19 -0800

    Create backup-vnstatDB.sh

---
+++ Router/backup-vnstatDB.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DATE=$(date +%m-%d-%Y)
+BACKUP_DIR="/etc/backup"
+ 
+# backup the vnstat db and add a timestamp to the file
+cp  /var/lib/vnstat/vnstat.db $BACKUP_DIR/vnstat.db-$DATE.bkp
+
+# Delete files older than 5 days #
+find $BACKUP_DIR/*.bkp -mtime +5 -exec rm {} \;
