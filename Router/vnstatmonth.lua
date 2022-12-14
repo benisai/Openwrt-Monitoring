dfca7ccd77412d1ee793a9146580b665295828ee
benisai <59147467+benisai@users.noreply.github.com>
2022-12-14 07:50:35 -0800

    Delete vnstatmonth.lua

--- Router/vnstatmonth.lua
+++
@@ -1,9 +0,0 @@
-local function scrape()
-  local vnstatmonth = space_split(get_contents("/tmp/vnstatmonth.out"))
-
-  metric("router_vnstat_month_download", "gauge", nil, vnstatmonth[1])
-  metric("router_vnstat_month_upload", "gauge", nil, vnstatmonth[2])
-
-end
-
-return { scrape = scrape }
