9ee063064bf0effe2244c6a5b267cad795c2339d
benisai <59147467+benisai@users.noreply.github.com>
2022-12-13 18:53:48 -0800

    Update device-status-ping.sh

--- Router/device-status-ping.sh
+++ Router/device-status-ping.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 IFS=$IFS
-while read mac ip name; do
+while read time mac ip name bs; do
     ping -c1 "$ip" &>/dev/null && echo device=$name mac=$mac ip=$ip status=online || echo device=$name mac=$mac ip=$ip status=offline
-done < /etc/config/knownmac.txt > /tmp/device-status.out
+done < /tmp/dhcp.leases > /tmp/device-status.out
