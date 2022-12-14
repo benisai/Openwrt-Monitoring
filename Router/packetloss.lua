e3d55eb71914aa292b8113310be0e30ec3d0964b
benisai <59147467+benisai@users.noreply.github.com>
2022-12-14 07:49:31 -0800

    Delete packetloss.lua

--- Router/packetloss.lua
+++
@@ -1,8 +0,0 @@
-local function scrape()
-  local packetloss = space_split(get_contents("/tmp/packetloss.out"))
-
-  metric("packet_loss", "gauge", nil, packetloss[1])
-
-end
-
-return { scrape = scrape }
