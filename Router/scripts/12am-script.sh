c0ff809ff26d656190aca31dbf1678f951560a73
benisai <59147467+benisai@users.noreply.github.com>
2022-12-14 07:25:24 -0800

    Create 12am-script.sh

---
+++ Router/scripts/12am-script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DATE=$(date +%m-%d-%Y)
+BACKUP_DIR="/etc/backup"
+
+# backup the vnstat.db and bwmon.db and add a timestamp to the file
+cp  /var/lib/vnstat/vnstat.db $BACKUP_DIR/vnstat.db-$DATE.bkp
+cp  /var/lib/vnstat/vnstat.db /tmp/mountd/disk1_part1/vnstat
+cp  /tmp/usage.db $BACKUP_DIR/usage.db-$DATE.bkp
+
+# Delete vnstat backup files older than 3 days #
+find $BACKUP_DIR/*.bkp -mtime +3 -exec rm {} \;
+find /tmp/mountd/disk1_part1/vnstat/*.bkp -mtime +3 -exec rm {} \;
+
+#Remove new_device file
+rm /tmp/new_device.out
+
+#NOTE: Please update the interface so it matches your router. 
+# If its the first of the month, it will drop the interface. 
+if [ `date +%d` = "01" ]
+then
+    vnstat --remove -i wlan-sta0 --force
+    vnstat --add -i wlan-sta0
+    /etc/init.d/vnstat restart
+
+else
+    echo "not first of the month"
+fi
