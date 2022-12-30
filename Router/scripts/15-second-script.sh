d2f04b88933b43ed15a0239c1be88538558400bd
benisai <59147467+benisai@users.noreply.github.com>
2022-12-29 17:21:43 -0800

    Update 15-second-script.sh

--- Router/scripts/15-second-script.sh
+++ Router/scripts/15-second-script.sh
@@ -5,7 +5,7 @@
 wrtbwmon update /tmp/usage.db
 wrtbwmon publish /tmp/usage.db /tmp/usage.htm
 sleep 1
-cat /tmp/usage.htm | grep "2022" | sed 's/,/  /g; s/"//g; s/new Array//g' | tr -d '()' | sed '$d' > /tmp/bwmon_usage.out
+cat /tmp/usage.htm | grep "2023" | sed 's/,/  /g; s/"//g; s/new Array//g' | tr -d '()' | sed '$d' > /tmp/bwmon_usage.out
 
 sleep 1
 #Get Temp and Fan speed for GL-Routers
