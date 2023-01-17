c2f28d9a6bee394c19694e96103ed6ec3ed0065b
benisai <59147467+benisai@users.noreply.github.com>
2023-01-16 19:19:46 -0800

    Update new_device.sh

--- Router/scripts/new_device.sh
+++ Router/scripts/new_device.sh
@@ -4,4 +4,4 @@ logread | grep "received args: add" | awk '{print $1, $2, $3, $4, $11, $12, $13}
 IFS=$IFS
 while read day m d t mac ip name; do
     echo date=$day-$m-$d-$t device=$name mac=$mac ip=$ip
-done < /tmp/newd.tmp > /tmp/new-device.txt
+done < /tmp/newd.tmp > /tmp/new_device.out
