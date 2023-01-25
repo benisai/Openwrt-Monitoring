725f90d864d70b4f6e392ab38d9d63379f051de5
benisai <59147467+benisai@users.noreply.github.com>
2023-01-25 12:47:02 -0800

    Update netify-log.sh

--- Docker/netify-log.sh
+++ Docker/netify-log.sh
@@ -6,7 +6,7 @@ if [ -z "$PIDS" ]; then
   echo "Starting Netify"
   sleep 1000 | nc 10.0.5.1 7150 | grep established  >> /var/log/netify.log &
   PIDS=`ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}'`
-  echo new PID $PIDS
+  echo new PID is $PIDS
   exit 1
 else
   for PID in $PIDS; do
