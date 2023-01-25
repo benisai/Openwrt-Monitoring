01317a123f726c0be998a8fe81f6c7145f962216
benisai <59147467+benisai@users.noreply.github.com>
2023-01-25 13:45:53 -0800

    Update netify-log.sh

--- Docker/netify-log.sh
+++ Docker/netify-log.sh
@@ -4,7 +4,9 @@ PIDS=`ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}'`
 if [ -z "$PIDS" ]; then
   echo "Process not running." 1>&2
   echo "Starting Netify"
-  sleep 1000 | nc 10.0.5.1 7150 | grep established  >> /var/log/netify.log &
+  #sleep 1000 | nc 10.0.5.1 7150 | grep established  >> /var/log/netify.log &
+  #sleep 1000 | nc 10.0.5.1 7150 | grep established | tr -d '"' | sed 's/:/=/g; s/,/ /g'  >> /var/log/netify.log &
+  sleep 1000 | nc 10.0.5.1 7150 | grep established | sed 's/"established":false,//g; s/"flow":{//g; s/0}/0/g'   >> /var/log/netify.log &
   PIDS=`ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}'`
   echo new PID is $PIDS
   exit 1
