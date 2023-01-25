48fa42269e8e525f033547121ed9b760e4633fe6
benisai <59147467+benisai@users.noreply.github.com>
2023-01-25 15:46:43 -0800

    Update netify-log.sh

--- Docker/netify-log.sh
+++ Docker/netify-log.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
-PIDS=`ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}'`
 
+#########################--Kill any existing NetCat--#########################
+PID=`ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}'`
+if test -d /proc/"$PID"/; then
+    echo "Netify Netcat Process Found, killing Process" $PID
+    kill -9 $PID 1>&2
+fi
+
+#########################--Start NetCat--#########################
+PIDS=`ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}'`
 if [ -z "$PIDS" ]; then
-  echo "Process not running." 1>&2
-  echo "Starting Netify"
-  #sleep 1000 | nc 10.0.5.1 7150 | grep established  >> /var/log/netify.log &
-  #sleep 1000 | nc 10.0.5.1 7150 | grep established | tr -d '"' | sed 's/:/=/g; s/,/ /g'  >> /var/log/netify.log &
-  sleep 1000 | nc 10.0.5.1 7150 | grep established | sed 's/"established":false,//g; s/"flow":{//g; s/0}/0/g'   >> /var/log/netify.log &
+  echo "Netify Netcat Process is Not Running." 1>&2
+  echo "Starting Netify Netcat Process"
+  sleep 1000 | nc 10.0.5.1 7150 | grep established | sed 's/"established":false,//g; s/"flow":{//g; s/0}/0/g'   >> /var/log/netify/netify.log &
   PIDS=`ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}'`
-  echo new PID is $PIDS
+  echo New Netify Netcat PID $PIDS
   exit 1
 else
   for PID in $PIDS; do
