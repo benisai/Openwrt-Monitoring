f7ccb6620866ea6ee7f878c8d73a82b0d9d8ee3c
benisai <59147467+benisai@users.noreply.github.com>
2023-01-28 08:55:01 -0800

    Update 5-minute-script.sh

--- Router/scripts/5-minute-script.sh
+++ Router/scripts/5-minute-script.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-#service netifyd restart
+service netifyd restart
 
-###-----Restart Netifyd as it uses alot of memory---------###
-#PID=`ps | grep "netifyd" | grep -v "grep" | awk '{print $1}'`
-#if test -d /proc/$PID/; then
-#    echo Netify Netcat Process Found, killing Process $PID
-#    kill -9 $PID 1>&2
-#fi
-###--------------------------------------------------------###
+
