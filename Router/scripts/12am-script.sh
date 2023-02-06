b303ab759ff58519532922f45fde3d3d7a08c732
benisai <59147467+benisai@users.noreply.github.com>
2023-02-05 16:50:00 -0800

    Update 12am-script.sh

--- Router/scripts/12am-script.sh
+++ Router/scripts/12am-script.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 DATE=$(date +%m-%d-%Y)
 BACKUP_DIR="/etc/backup"
+BACKUP_DIR="/tmp/mountd/disk1_part1"
 
 # backup the vnstat.db and bwmon.db and add a timestamp to the file
 cp  /var/lib/vnstat/vnstat.db $BACKUP_DIR/vnstat.db-$DATE.bkp
-cp  /var/lib/vnstat/vnstat.db /tmp/mountd/disk1_part1/vnstat
 cp  /tmp/usage.db $BACKUP_DIR/usage.db-$DATE.bkp
 
 # Delete vnstat backup files older than 3 days #
 find $BACKUP_DIR/*.bkp -mtime +3 -exec rm {} \;
-find /tmp/mountd/disk1_part1/vnstat/*.bkp -mtime +3 -exec rm {} \;
 
 #Remove new_device file
 rm /tmp/new_device.out
+touch /tmp/new_device.out
 
-#NOTE: Please update the interface so it matches your router. 
-# If its the first of the month, it will drop the interface. 
-if [ `date +%d` = "01" ]
+#NOTE: Please update the interface so it matches your router.
+# If its the first of the month, it will drop the interface.
+
+bi=$(date +%d)
+if [ $bi = "05" ]
 then
     vnstat --remove -i wlan-sta0 --force
     vnstat --add -i wlan-sta0
