754fec93f819320c24caf9654b23fb979b48d64b
benisai <59147467+benisai@users.noreply.github.com>
2023-02-12 20:10:04 -0800

    Update netify-log.sh

--- Docker/netify-log.sh
+++ Docker/netify-log.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-# Input file
+
+# Netify Log File Location
+FILE=/var/log/netify/netify.log
+# How many seconds before file is deemed "older"
+OLDTIME=30
+# Get current and file times
+CURTIME=$(date +%s)
+FILETIME=$(stat $FILE -c %Y)
+TIMEDIFF=$(expr $CURTIME - $FILETIME)
 
 ##########--Create Netify Folder and File---########
 if [ ! -f /var/log/netify/netify.log ]; then
@@ -7,31 +15,28 @@ if [ ! -f /var/log/netify/netify.log ]; then
   touch /var/log/netify/netify.log
 fi
 
-FILE=/var/log/netify/netify.log
-# How many seconds before file is deemed "older"
-OLDTIME=300
-# Get current and file times
-CURTIME=$(date +%s)
-FILETIME=$(stat $FILE -c %Y)
-TIMEDIFF=$(expr $CURTIME - $FILETIME)
-
 # Check if file older
 if [ $TIMEDIFF -gt $OLDTIME ]; then
   echo "File is older, restarting Netify.log"
   ##########################--Kill any existing NetCat--#########################
-  PID=$(ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}')
-  echo "Netify Netcat Was Found, PID: " $PID
-  if test -d /proc/"$PID"/; then
-    echo "Netify Netcat Process Found, killing Process" $PID
+  PIDS=$(ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}')
+  echo "Netify Netcat Was Found, PID: " $PIDS
+  for PID in $PIDS; do
+    echo "Killing Process" $PID
     kill -9 $PID 1>&2
-  fi
+  done
+
   #########################--Start NetCat--#####################################
   PIDS=$(ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}')
   if [ -z "$PIDS" ]; then
     echo "Netify Netcat Process is Not Running." 1>&2
     echo "Starting Netify Netcat Process"
-    sleep 1000 | nc 10.0.5.1 7150 | grep established | sed 's/"established":false,//g; s/"flow":{//g; s/0}/0/g' >>/var/log/netify/netify.log &
-    #sleep 1000 | nc 10.0.5.1 7150 | grep established | sed 's/"established":false,//g; s/"flow":{//g; s/0}/0/g; s/:/=/g; s/,/ /g; s/"//g'   >> /var/log/neti>
+    sleep 1000 | nc 10.0.5.1 7150 | grep established | sed 's/"established":false,//g; s/"flow":{//g; s/0}/0/g' | (sed '
+        #Replace with your IP/Hostname
+        s/10.0.5.120/AI-CAM/g;
+        s/10.0.5.121/LivingRoom-CAM/g;
+        s/10.0.5.113/tasmotaOL2-2268/g') >>/var/log/netify/netify.log &
+
     PIDS=$(ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}')
     echo New Netify Netcat PID $PIDS
     exit 1
@@ -41,5 +46,10 @@ if [ $TIMEDIFF -gt $OLDTIME ]; then
     done
   fi
 else
+  PIDS=$(ps -eaf | grep "10.0.5.1" | grep -v grep | awk '{print $2}')
+  echo "Netify PID: " $PIDS
+  for PID in $PIDS; do
+    echo "Netify Process" $PID
+  done
   echo Netify.log is current $FILETIME
 fi
