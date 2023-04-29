2ab7659a677ff1a827690808106937191a501275
benisai <59147467+benisai@users.noreply.github.com>
2023-04-28 20:19:08 -0700

    Update wan-monitor.sh

--- Router/scripts/wan-monitor.sh
+++ Router/scripts/wan-monitor.sh
@@ -5,7 +5,7 @@ internet_working=1
 start_time=0
 
 # Set the path to the log file
-log_file="./wan_monitor.log"
+log_file="/tmp/wan_monitor.log"
 
 if [ -f "$log_file" ]; then
   rm "$log_file"
@@ -14,7 +14,7 @@ fi
 # Start Process
 while true; do
     # Ping Google DNS server
-    ping -c 1 10.0.5.225 > /dev/null 2>&1
+    ping -c 1 8.8.8.8 > /dev/null 2>&1
 
     # Check if ping was successful
     if [ $? -eq 0 ]; then
@@ -22,20 +22,19 @@ while true; do
         if [ $internet_working -eq 0 ]; then
             end_time=$(date +%s)
             elapsed_time=$((end_time - start_time))
-            echo "$(date '+%Y-%m-%d %H:%M:%S') Internet is working again after $elapsed_time seconds" >> $log_file
+            echo "$(date '+%Y-%m-%d %H:%M:%S') Internet_is_working_again_after $elapsed_time" >> $log_file
             internet_working=1
         fi
     else
         # Internet is not working
         if [ $internet_working -eq 1 ]; then
             start_time=$(date +%s)
-            echo "$(date '+%Y-%m-%d %H:%M:%S') Internet is down" >> $log_file
+            echo "$(date '+%Y-%m-%d %H:%M:%S') Internet_is down" >> $log_file
             internet_working=0
         fi
 
         # Increment seconds
         seconds=$((seconds+10))
-        #echo "$(date '+%Y-%m-%d %H:%M:%S') Seconds elapsed: $seconds" >> $log_file
     fi
 
     # Wait for 1 second before pinging again
