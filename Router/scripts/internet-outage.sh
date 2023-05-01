c44b8243a9b996f53c5ffb6b82cc465d01d6b628
benisai <59147467+benisai@users.noreply.github.com>
2023-05-01 15:26:54 -0700

    Update internet-outage.sh

--- Router/scripts/internet-outage.sh
+++ Router/scripts/internet-outage.sh
@@ -7,35 +7,39 @@ start_time=0
 # Set the path to the log file
 log_file="/tmp/wan_monitor.log"
 
-if [ -f "$log_file" ]; then
-  rm "$log_file"
-fi
+# Delete log file if it exists
+#if [ -f "$log_file" ]; then
+#  rm "$log_file"
+#fi
 
 while true; do
     # Ping Google DNS server
-    ping -c 1 10.0.5.225 > /dev/null 2>&1
+    ping -c 1 10.0.5.225 2>&1 > /dev/null
 
     # Check if ping was successful
     if [ $? -eq 0 ]; then
-        # Internet is working
+        # Internet is working (iiw)
         if [ $internet_working -eq 0 ]; then
             end_time=$(date +%s)
             elapsed_time=$((end_time - start_time))
-            echo "$(date '+%Y-%m-%d-%H:%M:%S') Internet_is_working $elapsed_time" >> $log_file
+            #Create a new file, with the down message and up message in 1 line.
+            upmsg="$(date '+%Y-%m-%d-%H:%M:%S') up $elapsed_time"
+            sed -i "s/999999/$upmsg/g" $log_file #> $log_file.tmp && mv $log_file.tmp $log_file
             internet_working=1
         fi
     else
-        # Internet is not working
+        # Internet is not working (iid)
         if [ $internet_working -eq 1 ]; then
             start_time=$(date +%s)
-            echo "$(date '+%Y-%m-%d-%H:%M:%S') Internet_is_down 999999" >> $log_file
+            echo "$(date '+%Y-%m-%d-%H:%M:%S') down 999999 " >> $log_file
             internet_working=0
         fi
 
         # Increment seconds
         seconds=$((seconds+10))
     fi
-
+    
+    
     # Wait for 1 second before pinging again
     sleep 1
 
