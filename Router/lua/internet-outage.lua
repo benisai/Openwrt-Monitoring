d373073f3727b378be0ed1ffb0d35520e26b99e1
benisai <59147467+benisai@users.noreply.github.com>
2023-05-01 15:26:12 -0700

    Update internet-outage.lua

--- Router/lua/internet-outage.lua
+++ Router/lua/internet-outage.lua
@@ -1,38 +1,35 @@
-local wan_mon = {
-    "date",
-    "message",
+local wanoutagestat = {
+    "downdate",
+    "downmsg",
+    "update",
+    "upmsg",
     "value"
 }
 
+local pattern = "(%d+-%d+-%d+%d+-+%d+:%d+:%d+)%s+(%S+)%s+(%d+-%d+-%d+%d+-+%d+:%d+:%d+)%s+(%S+)%s+(%d+)"
+
 local function scrape()
-    local wanmon_table = {}
-    local last_two_lines = {}
-
-    -- read the last two lines of the log file
-    for line in io.lines("/tmp/wan_monitor.log") do
-        table.insert(last_two_lines, line)
-        if #last_two_lines > 2 then
-            table.remove(last_two_lines, 1)
-        end
+  local wanoutage_table = {}
+  for line in io.lines("/tmp/wan_monitor.log") do
+    local t = {string.match(line, pattern)}
+    if #t == 5 then
+      wanoutage_table[t[1]] = t
     end
+  end
 
-    -- parse the last two lines of the log file
-    for _, line in ipairs(last_two_lines) do
-        local t = {string.match(line, "(%d+-%d+-%d+%d+-+%d+:%d+:%d+)%s+(%S+)%s+(%d+)")}
-        if #t == 3 then
-            table.insert(wanmon_table, t)
-        end
-    end
+  -- create a new metric
+  wanoutage_metric = metric("wanoutage_stats", "counter")
 
-    -- create a new metric
-    wanmon_metric = metric("wanmon_stats", "counter")
-
-    -- iterate through the wanmon_table and add the values to the metric
-    for _, wanmon in ipairs(wanmon_table) do
-        wanmon_metric({
-            date = wanmon[1],
-            message = wanmon[2],
-            value = wanmon[3],
-        }, wanmon[3])
-    end
+  -- iterate through the wanoutage_table and add the values to the metric
+  for downdate, wanoutage in pairs(wanoutage_table) do
+    wanoutage_metric({
+      downdate = wanoutage[1],
+      downmsg = wanoutage[2],
+      update = wanoutage[3],
+      upmsg = wanoutage[4],
+      value = wanoutage[5]
+    }, wanoutage[5])
+  end
 end
+
+return { scrape = scrape }
