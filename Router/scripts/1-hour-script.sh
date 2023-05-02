af389348e74482692dfcd4b5859cd569ff2e6dc0
benisai <59147467+benisai@users.noreply.github.com>
2023-05-01 18:11:49 -0700

    Update 1-hour-script.sh

--- Router/scripts/1-hour-script.sh
+++ Router/scripts/1-hour-script.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+#---------------------------------------------------------------------------------------------------------#
 #service netifyd restart
-
 PID=`ps | grep "netifyd" | grep -v "grep" | awk '{print $1}'`
-
 if test -d /proc/$PID/; then
     echo Netify Netcat Process Found, killing Process $PID
     kill -9 $PID 1>&2
 fi
+
+#---------------------------------------------------------------------------------------------------------#
