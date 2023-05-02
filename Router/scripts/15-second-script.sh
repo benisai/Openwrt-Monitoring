40c3c1107f8a4a9810e788041205fb860dccb842
benisai <59147467+benisai@users.noreply.github.com>
2023-05-01 18:13:04 -0700

    Update 15-second-script.sh

--- Router/scripts/15-second-script.sh
+++ Router/scripts/15-second-script.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 # set -x
-
+#---------------------------------------------------------------------------------------------------------#
 #wrtbwmon script will run every
 wrtbwmon update /tmp/usage.db
 wrtbwmon publish /tmp/usage.db /tmp/usage.htm
 cat /tmp/usage.htm | grep "2023" | sed 's/,/  /g; s/"//g; s/new Array//g' | tr -d '()' | sed '$d' > /tmp/bwmon_usage.out
 
+#---------------------------------------------------------------------------------------------------------#
 #Get Temp and Fan speed for GL-Routers
 temp=`cat /sys/class/thermal/thermal_zone0/temp`
 speed=`gl_fan -s`
 echo "$temp $speed" > /tmp/tempstats.out
 
+#---------------------------------------------------------------------------------------------------------#
