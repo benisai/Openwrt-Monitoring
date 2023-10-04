2053791b1251d523b917290ca5d42870e1a34c8b
benisai <59147467+benisai@users.noreply.github.com>
2023-10-03 18:55:31 -0700

    Update serverSetup.sh
    updated crontab config

--- serverSetup.sh
+++ serverSetup.sh
@@ -31,6 +31,8 @@ if ! command -v docker &> /dev/null; then
         echo "running apt update and apt install docker-compose"
         sudo apt update
         sudo apt install docker-compose -y
+        # Create a Docker network
+        sudo docker network create internal
     else
         echo "Docker installation aborted. Continuing with the script."
         # No exit here, script will continue
@@ -55,20 +57,11 @@ sed -i "s/10.0.5.1/$ROUTER_IP/g" prometheus.yml
 # Replace the Router IP in netify-log.sh
 sed -i "s/10.0.5.1/$ROUTER_IP/g" netify-log.sh
 
-# Create a Docker network
-sudo docker network create internal
-
 # Start the Docker containers
 sudo docker-compose up -d
-
-# Create a Crontab entry
-echo "*/1 * * * * /home/$CURRENT_USER/Openwrt-Monitoring/Docker/netify-log.sh >> /var/log/crontab.netify.txt 2>&1" > cronjob.tmp
 
 # Make netify-log.sh executable
 sudo chmod +x netify-log.sh
 
-# Edit netify-log.sh
-#sudo nano netify-log.sh
-
-# Clean up temporary files
-rm cronjob.tmp
+# Create a Crontab entry as root
+sudo bash -c "echo '*/1 * * * * /home/$CURRENT_USER/Openwrt-Monitoring/Docker/netify-log.sh >> /var/log/crontab.netify.txt 2>&1' > /etc/cron.d/netify-log-cronjob"
